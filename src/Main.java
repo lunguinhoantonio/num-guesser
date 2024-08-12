e291a5a7b4dae58cdfe631fa658bbfd65d97c36b
Antonio Lunguinho <antoniolunguinho007@gmail.com>
2024-08-11 22:05:06 -0300

    Added: number tips

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String negativeMsg;
+        String tipMsg;
+
         double num = Math.random();
         int randomNum = (int) (1 + num * (5 - 1));
         int attempts = 2;
@@ -13,13 +15,16 @@ public class Main {
             System.out.print("Guess a number between 1 and 5: ");
             userAttempt = scan.nextInt();
             if (userAttempt == randomNum) {
+                System.out.println("--------------------------------");
                 System.out.print("You win!");
                 System.exit(0);
             } else {
                 --attempts;
                 System.out.println("--------------------------------");
                 negativeMsg = (attempts != 0) ? "Try again!" : "You lose! \nThe correct number was " + randomNum;
+                tipMsg = ((userAttempt < randomNum) && attempts != 0) ? "The number is bigger" : "The number is smaller";
                 System.out.println(negativeMsg);
+                System.out.println(tipMsg);
             }
         }
         while (attempts > 0);
